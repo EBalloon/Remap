d6eb751b91a7b07775cf44b2122b17ab22dfaac6
EBalloon <supergames2k@hotmail.com>
2022-01-24 10:18:55 +0000

    Remap

--- Remap/Remap.cpp
+++ Remap/Remap.cpp
@@ -4,7 +4,7 @@
 #include "kdmapper/kdmapper.hpp"
 #include "RawData.h"
 #include "lazyimporter.h"
-#include "oxorany.h"
+#include "SkCypter.h"
 
 constexpr ULONG CTL_Remap = 0x0001;
 constexpr ULONG CTL_Restore = 0x0002;
@@ -28,10 +28,10 @@ public:
 
 		hDevice = iat(CreateFileW)(DeviceName, DWORD(0), FILE_SHARE_READ | FILE_SHARE_WRITE, LPSECURITY_ATTRIBUTES(nullptr), OPEN_EXISTING, DWORD(0), HANDLE(0));
 		if (hDevice == INVALID_HANDLE_VALUE) {
-			return oxorany(false);
+			return false;
 		}
 
-		return oxorany(true);
+		return true;
 	}
 
 	bool DriverIsRunning(ULONG CTL_CODE) {
@@ -52,15 +52,15 @@ public:
 		Info.TargetPid = TargetPid;
 		Info.SourcePid = SourcePid;
 		Info.Code = CTL_CODE;
-		Info.RequestID = oxorany(0xDEADFFFF);
+		Info.RequestID = 0xDEADFFFF;
 
 		IO_STATUS_BLOCK IOBlock = { 0, 0 };
 		auto status = iat(NtDeviceIoControlFile)(hDevice, HANDLE(0), PIO_APC_ROUTINE(0), PVOID(0), &IOBlock, IOCTL_DISK_GET_DRIVE_GEOMETRY, &Info, sizeof(info_t), &Info, sizeof(info_t));
 		if (!NT_SUCCESS(status)) {
-			return oxorany(false);
+			return false;
 		}
 
-		return oxorany(true);
+		return true;
 	}
 
 	DWORD AttachProcess(std::string processName) {
@@ -115,9 +115,9 @@ int GetParam(const int argc, char** argv, const char* param) {
 int main(int argc, char* argv[])
 {
 	if (argc < 4) {
-		std::cout << oxorany("Usage: FakeProc GameProc Remap/Restore") << std::endl;
+		std::cout << skCrypt("Usage: FakeProc GameProc Remap/Restore") << std::endl;
 		system("pause");
-		return oxorany(0);
+		return 0;
 	}
 
 	bool Isret = false;
@@ -125,80 +125,80 @@ int main(int argc, char* argv[])
 
 	SetUnhandledExceptionFilter(SimplestCrashHandler);
 
-	Isret = Common.OpenDriver(oxorany(L"\\\\.\\PEAUTH"));
+	Isret = Common.OpenDriver(skCrypt(L"\\\\.\\PEAUTH"));
 	if (!Isret)
 	{ 
-		std::cout << oxorany("[-] Failed to Open Driver") << std::endl;
+		std::cout << skCrypt("[-] Failed to Open Driver") << std::endl;
 		system("pause");
-		return oxorany(0);
+		return 0;
 	}
 
 	if (!Common.DriverIsRunning(CTL_IsRunning))
 	{
-		std::cout << oxorany("[+] Loading Driver... ") << std::endl;
+		std::cout << skCrypt("[+] Loading Driver... ") << std::endl;
 
 		iqvw64e_device_handle = intel_driver::Load();
 		if (iqvw64e_device_handle == INVALID_HANDLE_VALUE) {
 			system("pause");
-			return oxorany(0);
+			return 0;
 		}
 			
 		NTSTATUS exitCode = 0;
 		if (!kdmapper::MapDriver(iqvw64e_device_handle, rawData, sizeof(rawData), 0, 0, false, true, true, true, 0, &exitCode)) {
-			std::cout << oxorany("[-] Failed to LoadDriver") << std::endl;
+			std::cout << skCrypt("[-] Failed to LoadDriver") << std::endl;
 			intel_driver::Unload(iqvw64e_device_handle);
 			system("pause");
-			return oxorany(0);
+			return 0;
 		}
 
 		if (!NT_SUCCESS(exitCode)) {
-			std::cout << oxorany("[-] Failed to Open Driver") << std::endl;
+			std::cout << skCrypt("[-] Failed to Open Driver") << std::endl;
 			system("pause");
-			return oxorany(0);
+			return 0;
 		}
 	}
 	
 	if (Common.DriverIsRunning(CTL_IsRunning))
-		std::cout << oxorany("[+] Driver is Running") << std::endl;
+		std::cout << skCrypt("[+] Driver is Running") << std::endl;
 
 	auto notepad = Common.AttachProcess(argv[1]);
 	auto ProcessHacker = Common.AttachProcess(argv[2]);
 	if (!notepad || !ProcessHacker) {
-		std::cout << oxorany("[-] Failed to find process pid") << std::endl;
+		std::cout << skCrypt("[-] Failed to find process pid") << std::endl;
 		system("pause");
-		return oxorany(0);
+		return 0;
 	}
 
-	bool Remap = GetParam(argc, argv, oxorany("Remap")) > 0;
-	bool Restore = GetParam(argc, argv, oxorany("Restore")) > 0;
+	bool Remap = GetParam(argc, argv, skCrypt("Remap")) > 0;
+	bool Restore = GetParam(argc, argv, skCrypt("Restore")) > 0;
 
 	if (Remap)
 	{
 		Isret = Common.CallCode(CTL_Remap, notepad, ProcessHacker);
 		if (!Isret) {
-			std::cout << oxorany("[-] Failed to Call Code") << std::endl;
+			std::cout << skCrypt("[-] Failed to Call Code") << std::endl;
 			system("pause");
-			return oxorany(0);
+			return 0;
 		}
 
-		std::cout << oxorany("[+] Process was Remaped") << std::endl;
+		std::cout << skCrypt("[+] Process was Remaped") << std::endl;
 	}
 	else if (Restore)
 	{
 		Isret = Common.CallCode(CTL_Restore, 0, 0);
 		if (!Isret) {
-			std::cout << oxorany("[-] Failed to Call Code") << std::endl;
+			std::cout << skCrypt("[-] Failed to Call Code") << std::endl;
 			system("pause");
-			return oxorany(0);
+			return 0;
 		}
 
-		std::cout << oxorany("[+] Process was Restored") << std::endl;
+		std::cout << skCrypt("[+] Process was Restored") << std::endl;
 	}
 	else
 	{
-		std::cout << oxorany("[-] Wrong param") << std::endl;
+		std::cout << skCrypt("[-] Wrong param") << std::endl;
 	}
 
 	system("pause");
-	return oxorany(0);
+	return 0;
 }
